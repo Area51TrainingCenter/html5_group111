75b5a3931247a508b6380fd6f8f0c9c5eae872a6
Alumno <alumno@A51MM-04.local>
2018-12-22 14:55:55 -0500

    update

---
+++ Modulo01/clase02/formularios/registro.php
@@ -0,0 +1,7 @@
+$nombre=$_POST["nombres"]
+si nombre existe entonces guarda en base de datos
+si nombre no existe entonces regresalo al formulario
+guardarBD($nombre);
+redirecciono a la pagina de success
+
+si distrito=SJL ejecuta x tarea

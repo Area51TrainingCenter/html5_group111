fcc9e0ea209e193a3b88f80c35bea00207a97f65
Alumno <alumno@A51MM-04.local>
2019-02-16 13:58:45 -0500

    update

--- modulo03/clase02/ejercicio jquery/js/main.js
+++ modulo03/clase02/ejercicio jquery/js/main.js
@@ -1,5 +1,34 @@
+/*$("#item-nosotros").click(function(){
+	limpiar();
+	$("#item-nosotros").addClass("activo");
+})
 
+$("#item-servicios").click(function(){
+	limpiar();
+	$("#item-servicios").addClass("activo");
+})
+$("#item-contacto").click(function(){
+	limpiar();
+	$("#item-contacto").addClass("activo");
+})
+
+$("#item-blog").click(function(){
+	limpiar();
+	$("#item-blog").addClass("activo");
+})
+*/
 $(".nav-site ul li").click(function(){
-	$(".nav-site ul li").removeClass("activo");
+	limpiar();
 	$(this).addClass("activo")
 })
+
+
+function limpiar(){
+	$(".nav-site ul li").removeClass("activo");
+	/*$("#item-inicio").removeClass("activo");
+	$("#item-nosotros").removeClass("activo");
+	$("#item-blog").removeClass("activo");
+	$("#item-contacto").removeClass("activo");
+	$("#item-servicios").removeClass("activo");*/
+
+}
